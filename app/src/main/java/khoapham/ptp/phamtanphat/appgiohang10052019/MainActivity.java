1df10c8a43d64e9fe01358c15165ca3fd6bcea00
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-10 13:40:23 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/MainActivity.java
@@ -2,15 +2,22 @@ package khoapham.ptp.phamtanphat.appgiohang10052019;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageSwitcher;
+import android.widget.ImageView;
+import android.widget.ViewSwitcher;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageSwitcher imageSwitcher;
     Integer[] mangbaner = {R.drawable.baner1,R.drawable.baner2,R.drawable.baner3};
+    int i = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +30,30 @@ public class MainActivity extends AppCompatActivity {
 
         imageSwitcher.setInAnimation(in);
         imageSwitcher.setOutAnimation(out);
+
+        imageSwitcher.setFactory(new ViewSwitcher.ViewFactory() {
+            @Override
+            public View makeView() {
+                ImageView myView = new ImageView(MainActivity.this);
+                myView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                ImageSwitcher.LayoutParams params= new ImageSwitcher.LayoutParams(
+                        ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT);
+                myView.setLayoutParams(params);
+                return myView;
+            }
+        });
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (i >= mangbaner.length){
+                    i = 0;
+                }
+                imageSwitcher.setImageResource(mangbaner[i]);
+                i++;
+                handler.postDelayed(this,1000);
+            }
+        },1000);
+
     }
 }
