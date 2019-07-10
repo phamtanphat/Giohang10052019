2dd1e4531bda0e94cc5cc3e2fbf4a6e6ed0a52bc
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-10 13:15:02 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/MainActivity.java
@@ -3,12 +3,21 @@ package khoapham.ptp.phamtanphat.appgiohang10052019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageSwitcher;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageSwitcher imageSwitcher;
+    Integer[] mangbaner = {R.drawable.baner1,R.drawable.baner2,R.drawable.baner3};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageSwitcher = findViewById(R.id.imageswitcher);
+        Animation out= AnimationUtils.loadAnimation(this, android.R.anim.fade_out);
+        Animation in= AnimationUtils.loadAnimation(this, android.R.anim.fade_in);
     }
 }
