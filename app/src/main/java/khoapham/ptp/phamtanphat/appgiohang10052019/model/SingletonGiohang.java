992d30ae3859a135393c40caa8351374b2d9b2bd
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-12 14:05:00 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/model/SingletonGiohang.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/model/SingletonGiohang.java
@@ -15,7 +15,13 @@ public class SingletonGiohang {
         }
         return instance;
     }
-    public void setgiohang(int index , Dienthoai dienthoai){
+    public void xoasanpham(int index ){
+        mangdienthoai.remove(index);
+    }
+    public void themsanpham(Dienthoai dienthoai){
+        mangdienthoai.add(dienthoai);
+    }
+    public void capnhatsanpham(int index , Dienthoai dienthoai){
         mangdienthoai.set(index,dienthoai);
     }
     public ArrayList<Dienthoai> getGiohang(){
