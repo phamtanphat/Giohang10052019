e5035643f2e9956af46296c7544e640e6ec1ae95
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-15 20:34:17 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/adapter/GiohangAdapter.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/adapter/GiohangAdapter.java
@@ -1,16 +1,57 @@
 package khoapham.ptp.phamtanphat.appgiohang10052019.adapter;
 
+import android.content.Intent;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class GiohangAdapter {
+import java.text.DecimalFormat;
+
+import khoapham.ptp.phamtanphat.appgiohang10052019.R;
+import khoapham.ptp.phamtanphat.appgiohang10052019.activity.GiohangActivity;
+import khoapham.ptp.phamtanphat.appgiohang10052019.model.Dienthoai;
+import khoapham.ptp.phamtanphat.appgiohang10052019.model.SingletonGiohang;
+
+public class GiohangAdapter extends RecyclerView.Adapter<GiohangAdapter.Viewholder> {
+
+    @NonNull
+    @Override
+    public Viewholder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());
+        View view = layoutInflater.inflate(R.layout.dong_item_giohang,parent,false);
+        return new Viewholder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Viewholder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        if (SingletonGiohang.getInstance().getGiohang().size() <= 0){
+            return 0;
+        }
+        return SingletonGiohang.getInstance().getGiohang().size() ;
+    }
 
     class Viewholder extends RecyclerView.ViewHolder{
-
+        ImageView imgCong,imgTru,imgdienthoai;
+        TextView txtTen,txtGia,txtSoluong;
         public Viewholder(@NonNull View itemView) {
             super(itemView);
+            imgCong = itemView.findViewById(R.id.imageviewGiohangPlus);
+            imgTru = itemView.findViewById(R.id.imageviewGiohangMinus);
+            imgdienthoai = itemView.findViewById(R.id.imageviewGiohang);
+            txtTen = itemView.findViewById(R.id.textviewGiohangTendienthoai);
+            txtGia = itemView.findViewById(R.id.textviewGiohangGiadienthoai);
+            txtSoluong = itemView.findViewById(R.id.textviewGiohangSoluongdienthoai);
+
         }
     }
 }
