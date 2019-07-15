e024e6e29d152af6768c7af77ab8bc533dbd25f4
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-15 20:22:45 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/activity/GiohangActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/appgiohang10052019/activity/GiohangActivity.java
@@ -1,16 +1,26 @@
 package khoapham.ptp.phamtanphat.appgiohang10052019.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import khoapham.ptp.phamtanphat.appgiohang10052019.R;
 
 public class GiohangActivity extends AppCompatActivity {
 
+    RecyclerView recyclerViewGiohang;
+    TextView txtTongtien;
+    Button btnThanhtoan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_giohang);
+
+        recyclerViewGiohang = findViewById(R.id.recyclerviewGiohang);
+        txtTongtien = findViewById(R.id.textviewTongtien);
+        btnThanhtoan = findViewById(R.id.buttonThanhtoan);
     }
 }
